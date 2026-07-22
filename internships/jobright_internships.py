5404f2fd85fd2367efa808a12db2e80e57c71e9e
Vinny456 <vincent03280608@gmail.com>
2026-07-22 16:37:29 -0400

    fixed a small bug with the scraper not working

--- internships/jobright_internships.py
+++ internships/jobright_internships.py
@@ -1,3 +1,5 @@
+import os, sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from dotenv import load_dotenv
 from github import Github, Auth
 import os
--- new_grads/jobright_new_grad.py
+++ new_grads/jobright_new_grad.py
@@ -1,3 +1,5 @@
+import os, sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from internships.jobright_internships import JobrightInternships
 
 
